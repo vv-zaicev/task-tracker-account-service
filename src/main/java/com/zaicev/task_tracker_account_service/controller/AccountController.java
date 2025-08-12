e4ae0ab4d0229ddb34c33d8582fb15a04376487d
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-08-12 21:35:13 +0600

    ВÑреализованы эндпоинты для работы с аватаркаÐми

--- src/main/java/com/zaicev/task_tracker_account_service/controller/AccountController.java
+++ src/main/java/com/zaicev/task_tracker_account_service/controller/AccountController.java
@@ -7,13 +7,18 @@ import com.zaicev.task_tracker_account_service.dto.NamesResponse;
 import com.zaicev.task_tracker_account_service.mapper.AccountMapper;
 import com.zaicev.task_tracker_account_service.model.Account;
 import com.zaicev.task_tracker_account_service.service.AccountService;
+import com.zaicev.task_tracker_account_service.service.AvatarService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.Instant;
 
@@ -24,6 +29,8 @@ public class AccountController {
 
     private final AccountService accountService;
 
+    private final AvatarService avatarService;
+
     private final AccountMapper accountMapper;
 
     @GetMapping("/me")
@@ -33,12 +40,28 @@ public class AccountController {
     }
 
     @GetMapping("/me/names")
-    public NamesResponse getMeNames(@AuthenticationPrincipal Jwt jwt) {
+    public NamesResponse getMyNames(@AuthenticationPrincipal Jwt jwt) {
         String userId = jwt.getClaimAsString("userId");
         return accountMapper.toNamesResponse(accountService.getAccountById(Long.valueOf(userId)));
     }
 
-    @GetMapping()
+    @GetMapping("/me/avatar")
+    public ResponseEntity<ByteArrayResource> getMyAvatar(@AuthenticationPrincipal Jwt jwt) throws Exception {
+        Long userId = Long.valueOf(jwt.getClaimAsString("userId"));
+        ByteArrayResource avatar = avatarService.getAvatar(userId);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=avatar%d.jpg".formatted(userId))
+                .body(avatar);
+    }
+
+    @PostMapping("/me/avatar")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void uploadMyAvatar(@AuthenticationPrincipal Jwt jwt, @RequestParam("file") MultipartFile file) throws  Exception{
+        Long userId = Long.valueOf(jwt.getClaimAsString("userId"));
+        avatarService.saveAvatar(userId, file);
+    }
+
+    @GetMapping("/for-notifications")
     public AccountForNotificationPageResponse getAccountsForNotification(@RequestParam(defaultValue = "0") int page){
         Page<Account> accountPage = accountService.getAccountsForNotification(page);
         return accountMapper.toAccountForNotificationPageResponse(accountPage);
--- src/test/java/com/zaicev/task_tracker_account_service/controller/AccountControllerTest.java
+++ src/test/java/com/zaicev/task_tracker_account_service/controller/AccountControllerTest.java
@@ -5,22 +5,32 @@ import com.zaicev.task_tracker_account_service.mapper.AccountMapper;
 import com.zaicev.task_tracker_account_service.model.Account;
 import com.zaicev.task_tracker_account_service.model.NotificationType;
 import com.zaicev.task_tracker_account_service.service.AccountService;
+import com.zaicev.task_tracker_account_service.service.AvatarService;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AccountController.class)
@@ -34,9 +44,12 @@ public class AccountControllerTest {
     @MockitoBean
     private AccountMapper accountMapper;
 
+    @MockitoBean
+    private AvatarService avatarService;
+
 
     @Test
-    void getMe_success() throws Exception {
+    void getMe_ShouldReturnOKWithCorrectBody() throws Exception {
         Account account = new Account();
         AccountResponse accountResponse = AccountResponse.builder()
                 .id(1L)
@@ -49,8 +62,8 @@ public class AccountControllerTest {
                 .accountSetting(new AccountSettingResponse(true, NotificationType.EMAIL))
                 .build();
 
-        Mockito.when(accountService.getAccountById(1L)).thenReturn(account);
-        Mockito.when(accountMapper.toAccountResponse(account)).thenReturn(accountResponse);
+        when(accountService.getAccountById(1L)).thenReturn(account);
+        when(accountMapper.toAccountResponse(account)).thenReturn(accountResponse);
 
         mockMvc.perform(get("/account/me")
                         .with(jwt().jwt(jwt -> jwt.claim("userId", "1"))))
@@ -69,9 +82,9 @@ public class AccountControllerTest {
     }
 
     @Test
-    void getMe_WhenAccountNotFound_ReturnNotFound() throws Exception{
+    void getMe_WhenAccountNotFound_ShouldReturnNotFound() throws Exception {
         Exception exception = new EntityNotFoundException("Account not found");
-        Mockito.when(accountService.getAccountById(1L)).thenThrow(exception);
+        when(accountService.getAccountById(1L)).thenThrow(exception);
 
         mockMvc.perform(get("/account/me")
                         .with(jwt().jwt(jwt -> jwt.claim("userId", "1"))))
@@ -82,12 +95,12 @@ public class AccountControllerTest {
     }
 
     @Test
-    void getMeNames_success() throws Exception {
+    void getMeNames_ShouldReturnOK() throws Exception {
         Account account = new Account();
         NamesResponse namesResponse = new NamesResponse("firstName", "lastName");
 
-        Mockito.when(accountService.getAccountById(1L)).thenReturn(account);
-        Mockito.when(accountMapper.toNamesResponse(account)).thenReturn(namesResponse);
+        when(accountService.getAccountById(1L)).thenReturn(account);
+        when(accountMapper.toNamesResponse(account)).thenReturn(namesResponse);
 
         mockMvc.perform(get("/account/me/names")
                         .with(jwt().jwt(jwt -> jwt.claim("userId", "1"))))
@@ -96,17 +109,17 @@ public class AccountControllerTest {
                 .andExpect(jsonPath("$.firstName").value(namesResponse.getFirstName()))
                 .andExpect(jsonPath("$.lastName").value(namesResponse.getLastName()));
 
-        Mockito.verify(accountService).getAccountById(1L);
-        Mockito.verify(accountMapper).toNamesResponse(account);
+        verify(accountService).getAccountById(1L);
+        verify(accountMapper).toNamesResponse(account);
     }
 
     @Test
-    void getMeNames_WhenAccountNotFound_ReturnNotFound() throws Exception{
+    void getMyAvatar_WhenAvatarNotFound_ShouldReturnNotFound() throws Exception {
         Exception exception = new EntityNotFoundException("Account not found");
-        Mockito.when(accountService.getAccountById(1L)).thenThrow(exception);
+        when(avatarService.getAvatar(1L)).thenThrow(exception);
 
-        mockMvc.perform(get("/account/me/names")
-                .with(jwt().jwt(jwt -> jwt.claim("userId", "1"))))
+        mockMvc.perform(get("/account/me/avatar")
+                        .with(jwt().jwt(jwt -> jwt.claim("userId", "1"))))
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.statusCode").value(HttpStatus.NOT_FOUND.value()))
                 .andExpect(jsonPath("$.message").value(exception.getMessage()))
@@ -115,7 +128,52 @@ public class AccountControllerTest {
     }
 
     @Test
-    void getAccountsForNotification_success() throws Exception {
+    void getMyAvatar_ShouldReturnAvatarWithCorrectHeaders() throws Exception {
+        Long userId = 42L;
+        byte[] imageBytes = "fakeImage".getBytes();
+        ByteArrayResource resource = new ByteArrayResource(imageBytes);
+        when(avatarService.getAvatar(userId)).thenReturn(resource);
+
+        mockMvc.perform(get("/account/me/avatar")
+                        .with(jwt().jwt(jwt -> jwt.claim("userId", userId))))
+                .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=avatar42.jpg"))
+                .andExpect(content().bytes(imageBytes));
+    }
+
+    @Test
+    void getMeNames_WhenAccountNotFound_ShouldReturnNotFound() throws Exception {
+        Exception exception = new EntityNotFoundException("Account not found");
+        when(accountService.getAccountById(1L)).thenThrow(exception);
+
+        mockMvc.perform(get("/account/me/names")
+                        .with(jwt().jwt(jwt -> jwt.claim("userId", "1"))))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.statusCode").value(HttpStatus.NOT_FOUND.value()))
+                .andExpect(jsonPath("$.message").value(exception.getMessage()))
+                .andExpect(jsonPath("$.timestamp").exists());
+
+    }
+
+    @Test
+    void uploadMyAvatar_ShouldCallServiceAndReturn201() throws Exception {
+        Long userId = 42L;
+        MockMultipartFile file = new MockMultipartFile(
+                "file", "avatar.jpg", "image/jpeg", "fakeImage".getBytes()
+        );
+
+        // when / then
+        mockMvc.perform(multipart("/account/me/avatar")
+                        .file(file)
+                        .with(jwt().jwt(jwt -> jwt.claim("userId", userId))))
+                .andExpect(status().isCreated());
+
+        ArgumentCaptor<MultipartFile> fileCaptor = ArgumentCaptor.forClass(MultipartFile.class);
+        verify(avatarService).saveAvatar(ArgumentMatchers.eq(userId), fileCaptor.capture());
+    }
+
+    @Test
+    void getAccountsForNotification_ShouldReturnOK() throws Exception {
         Account account = new Account();
         List<Account> accounts = List.of(account);
         Page<Account> accountPage = new PageImpl<>(accounts);
@@ -132,10 +190,10 @@ public class AccountControllerTest {
                 false // no next page
         );
 
-        Mockito.when(accountService.getAccountsForNotification(0)).thenReturn(accountPage);
-        Mockito.when(accountMapper.toAccountForNotificationPageResponse(accountPage)).thenReturn(pageResponse);
+        when(accountService.getAccountsForNotification(0)).thenReturn(accountPage);
+        when(accountMapper.toAccountForNotificationPageResponse(accountPage)).thenReturn(pageResponse);
 
-        mockMvc.perform(get("/account")
+        mockMvc.perform(get("/account/for-notifications")
                         .param("page", "0")
                         .with(jwt().jwt(jwt -> jwt.claim("userId", "1"))))
                 .andExpect(status().isOk())
